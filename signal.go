3f194750115556f9f8c6a0ba4f3c948105352e4d
Kyoichiro Yamada <yamada@wacul.co.jp>
2016-12-12 23:23:52 +0900

    signal

---
+++ signal.go
@@ -0,0 +1,16 @@
+package batch
+
+import "golang.org/x/net/context"
+
+// Signal the Worker with the cancelation
+type Signal struct {
+	Worker func(context.Context)
+	Sig
+}
+
+// Run worker with the cancelable context
+func (s *Signal) Run(ctx context.Context) {
+	ctx, cancel := s.context(ctx)
+	defer cancel()
+	s.Worker(ctx)
+}
