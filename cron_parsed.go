cf8cc97571d9d68d2dbd030e001074f01a1d54ba
Kyoichiro Yamada <me@kyoh86.dev>
2020-04-06 17:57:37 +0900

    Support parallel usage for Cron. (#1)
    This commit is the patch for https://github.com/gorhill/cronexpr/issues/26.

--- cron_parsed.go
+++ cron_parsed.go
@@ -7,34 +7,17 @@ import (
 	"github.com/gorhill/cronexpr"
 )
 
-// Cron will process the Worker on the schedule
-type Cron struct {
+type cronParsed struct {
 	Worker    func(context.Context)
-	Expr      string
+	Expr      *cronexpr.Expression
 	Location  *time.Location
 	Immediate bool // Run the worker immidiately.
 	Once      bool // Run the worker at once.
 	Sig
 }
 
-// Compile parses CRON-expression and fix all properties
-func (c *Cron) Compile() (Batch, error) {
-	expr, err := cronexpr.Parse(c.Expr)
-	if err != nil {
-		return nil, err
-	}
-	return &cronParsed{
-		Worker:    c.Worker,
-		Expr:      expr,
-		Location:  c.Location,
-		Immediate: c.Immediate,
-		Once:      c.Once,
-		Sig:       c.Sig,
-	}, nil
-}
-
 // Run the Worker on the schedule
-func (c *Cron) Run(ctx context.Context) {
+func (c *cronParsed) Run(ctx context.Context) {
 	ctx, cancel := c.context(ctx)
 	defer cancel()
 
@@ -50,7 +33,7 @@ func (c *Cron) Run(ctx context.Context) {
 		}
 
 		now := time.Now().In(loc)
-		next := cronexpr.MustParse(c.Expr).Next(now)
+		next := c.Expr.Next(now)
 		wait := next.Sub(now)
 		if next.IsZero() {
 			return
